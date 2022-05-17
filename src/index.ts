7485dce44a15ed8a8b2c99fb57db519605d0228a
jianyun2020 <sujianyun2020@163.com>
2022-05-18 06:20:35 +0800

    Food类

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,33 @@
 // 引入样式
-import './style/index.less'
+import './style/index.less';
+
+// 定义食物类Food
+class Food {
+  // 定义一个属性表示食物所对应的元素
+  element: HTMLElement;
+
+  constructor() {
+    // 获取页面的food元素并将其赋值给element
+    this.element = document.getElementById('food')!;
+  }
+
+  // 定义一个获取食物X轴坐标的方法
+  get X() {
+    return this.element.offsetLeft;
+  }
+
+  // 定义一个获取食物Y轴坐标的方法
+  get Y() {
+    return this.element.offsetTop;
+  }
+  
+  // 修改食物的位置
+  change() {
+    // 食物的位置范围在0 ~ 290
+    const X = Math.round(Math.random() * 29) * 10;
+    const Y = Math.round(Math.random() * 29) * 10;
+
+    this.element.style.left = X + 'px';
+    this.element.style.top = Y + 'px';
+  }
+}
